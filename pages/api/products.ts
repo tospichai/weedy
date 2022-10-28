d9b0db022a3342b24a1b3463e30694653c98a5a7
Tospichai <tospichai@gmail.com>
2022-10-28 16:42:41 +0700

    add product and lang

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -5,7 +5,10 @@ const product = [
   {
     id: 1,
     name: "Wedding Cake",
-    desc: "Wedding Cake is a sweet strain, of course, but it packs a spicy aftermath (just like marriage). It’s great for calming down and going out, even for doing nothing",
+    desc: {
+      th: "Wedding Cake เป็นงานที่หวาน แต่กลับมีรสเผ็ด (เช่นเดียวกับการแต่งงาน) เหมาะสำหรับการสงบสติอารมณ์และออกไปข้างนอกแม้ไม่ได้ทำอะไรเลย",
+      en: "Wedding Cake is a sweet strain, of course, but it packs a spicy aftermath (just like marriage). It’s great for calming down and going out, even for doing nothing.",
+    },
     image: "http://localhost:3000/images/products/wedding-cake.png",
     thc: "24%",
     cbd: "1%",
@@ -14,11 +17,38 @@ const product = [
   {
     id: 2,
     name: "Ice Cream Mintz",
-    desc: "Ice Cream Mintz, also known as Ice Cream Mints,, is a indica weed strain made by crossing Ice Cream Cake and Kush Mints. Reviewers on Leafly say this strain makes them feel hungry, giggly, and happy.",
+    desc: {
+      th: "Ice Cream Mintz หรือที่เรียกว่า Ice Cream Mints เป็นสายพันธุ์วัชพืช indica ที่ทำโดยการข้าม Ice Cream Cake และ Kush Mints ผู้วิจารณ์บน Leafly กล่าวว่าความเครียดนี้ทำให้พวกเขารู้สึกหิว หัวเราะคิกคัก และมีความสุข",
+      en: "Ice Cream Mintz, also known as Ice Cream Mints,, is a indica weed strain made by crossing Ice Cream Cake and Kush Mints. Reviewers on Leafly say this strain makes them feel hungry, giggly, and happy.",
+    },
     image: "http://localhost:3000/images/products/ice-cream-mintz.png",
     thc: "23%",
     cbd: "0.6%",
     price: 300,
+  },
+  {
+    id: 3,
+    name: "GG4",
+    desc: {
+      th: "Original Glue หรือที่รู้จักในชื่อ Gorilla Glue, Original Glue, GG4 และ Gorilla Glue #4 เป็นกัญชาสายพันธุ์ไฮบริดที่มอบความรู้สึกสบายและผ่อนคลายให้กับมือหนักหน่วง ทำให้คุณรู้สึก “ติดกาว” กับโซฟา ดอกตูมที่เคลือบด้วยเรซินเป็นชิ้นๆ ของ Gorilla Glue เติมห้องด้วยกลิ่นดินและเปรี้ยวฉุนที่สืบทอดมาจากสายพันธุ์พ่อแม่",
+      en: "Original Glue, also known as Gorilla Glue, Original Glue, GG4, and Gorilla Glue #4 is a potent hybrid marijuana strain that delivers heavy-handed euphoria and relaxation, leaving you feeling “glued” to the couch. Gorilla Glue's chunky, resin-covered buds fill the room with pungent earthy and sour aromas inherited from its parent strains.",
+    },
+    image: "http://localhost:3000/images/products/gg-4.png",
+    thc: "20%",
+    cbd: "0%",
+    price: 450,
+  },
+  {
+    id: 4,
+    name: "Blue Dream",
+    desc: {
+      th: "บลูดรีมเป็นกัญชาสายพันธุ์ไฮบริดที่มี sativa เด่นโดยผสมบลูเบอร์รี่กับเฮซ สายพันธุ์นี้สร้างความสูงที่สมดุลพร้อมกับผลกระทบเช่นการกระตุ้นสมองและการผ่อนคลายร่างกายทั้งหมด ความฝันสีน้ำเงินคือ 18% THC แต่มีเปอร์เซ็นต์ CBD ต่ำ",
+      en: "Blue Dream is a sativa-dominant hybrid marijuana strain made by crossing Blueberry with Haze. This strain produces a balanced high, along with effects such as cerebral stimulation and full-body relaxation. Blue dream is 18% THC but has a low CBD percentage",
+    },
+    image: "http://localhost:3000/images/products/blue-dream.png",
+    thc: "18%",
+    cbd: "0%",
+    price: 350,
   },
 ];
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,11 @@ import Container from "../components/container";
 import Header from "../components/header";
 import Layout from "../components/layout";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { FormattedMessage } from "react-intl";
 
-export async function getStaticProps({ locale }) {
-  const res = await fetch(`http://localhost:3000/api/products?locale=${locale}`);
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/products");
   const products = await res.json();
 
   return {
@@ -16,7 +18,8 @@ export async function getStaticProps({ locale }) {
 }
 
 const Home: NextPage = ({ products }) => {
-
+  const router = useRouter();
+  const { locale } = router;
   return (
     <>
       <Layout>
@@ -24,22 +27,28 @@ const Home: NextPage = ({ products }) => {
           <title>Weedy</title>
         </Head>
         <Header />
-        <div className="bg-weed -mt-24 relative z-0 h-[600px] md:h-[1200px]"></div>
+        <div className="bg-weed -mt-24 relative z-0 h-[600px] md:h-[1000px]"></div>
         <div className="text-white -mt-0 md:-mt-96 relative flex justify-center md:justify-around">
           <div className="flex flex-col w-max text-center sm:text-left">
-            <div className="text-6xl md:text-8xl font-bold -mt-80">Don{"'"}t Panic,</div>
-            <div className="text-6xl md:text-8xl font-bold">It{"'"}s Organic.</div>
-            <div className="text-sm md:text-2xl font-thin mt-4">
-              High qualiuty medical and recreational cannabis with
+            <div className="text-6xl md:text-8xl font-bold -mt-80">
+              Don{"'"}t Panic,
             </div>
-            <div className="text-sm md:text-2xl font-thin">
-              fast and discreet delivery anywhere in the States
+            <div className="text-6xl md:text-8xl font-bold">
+              It{"'"}s Organic.
+            </div>
+            <div className="text-sm md:text-2xl font-thin mt-4">
+              <FormattedMessage
+                id="banner_desc"
+                values={{
+                  br: <br />,
+                }}
+              />
             </div>
             <Link href={"/shop"}>
               <a>
                 <div className="uppercase text-center mt-16">
                   <span className="bg-gradient-to-b from-green-500 to-green-700 p-4 rounded-lg text-2xl shadow-2xl shadow-green-400/50">
-                    Make order
+                    <FormattedMessage id="makeorder" />
                   </span>
                 </div>
               </a>
@@ -49,42 +58,48 @@ const Home: NextPage = ({ products }) => {
         </div>
         <Container>
           <div className="text-white relative z-10 flex justify-center mt-10 md:mt-96 flex-col text-center">
-            <div className="text-5xl font-bold">Featured products</div>
-            <div className="text-md sm:text-2xl font-thin mt-4">
-              Choose your favorite strains. Prices will be
+            <div className="text-5xl font-bold">
+              <FormattedMessage id="featured_products" />
             </div>
-            <div className="text-md sm:text-2xl font-thin">calculated upon checkout.</div>
+            <div className="text-md sm:text-2xl font-thin mt-4">
+              <FormattedMessage
+                id="featured_products_desc"
+                values={{
+                  br: <br />,
+                }}
+              />
+            </div>
           </div>
           <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8 gap-4">
             {products.map((item: any) => (
               <div
                 key={item.id}
-                className="border border-slate-800 hover:border-green-500 rounded-xl bg-secondary text-white"
+                className="border border-slate-800 hover:border-green-500 rounded-xl bg-secondary text-white flex flex-1 flex-col justify-between"
               >
-                <Image
-                  loader={() => item.image}
-                  src={item.image}
-                  width={500}
-                  height={500}
-                  alt={item.name}
-                />
-                <div className="p-6">
-                  <div className="flex justify-between flex-col">
+                <div>
+                  <Image
+                    loader={() => item.image}
+                    src={item.image}
+                    width={500}
+                    height={500}
+                    alt={item.name}
+                  />
+                  <div className="flex flex-1 flex-col justify-between p-6">
                     <div>
                       <div className="text-3xl">{item.name}</div>
                       <div className="font-thin text-lg text-slate-300 mt-4">
-                        {item.desc}
+                        {item.desc[locale]}
                       </div>
                     </div>
-                    <div>
-                      <div className="flex justify-between font-bold mt-4">
-                        <div>THC {item.thc}</div>
-                        <div>CBD {item.cbd}</div>
-                      </div>
-                      <div className="text-end mt-4 text-xl">
-                        {item.price}/g
-                      </div>
-                    </div>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="flex justify-between font-bold mt-4 p-6">
+                    <div>THC {item.thc}</div>
+                    <div>CBD {item.cbd}</div>
+                  </div>
+                  <div className="text-end m-6 text-xl">
+                    <FormattedMessage id="price" /> {item.price}/g
                   </div>
                 </div>
               </div>
